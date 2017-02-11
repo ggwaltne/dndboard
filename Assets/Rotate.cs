8348d46d3585dd66535fb09e43a22402b6a46471
Rakaim <kurtbomya@gmail.com>
2017-02-11 15:58:49 -0600

    Added rotation and shrunk hp radial

---
+++ Assets/Rotate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (0,0,50*Time.deltaTime); //rotates 50 degrees per second around z axis
+	}
+}
