99d7f812331a751abc3df0f32a4e4461d30e043c
Gage Gwaltney <ggwaltne@harding.edu>
2017-04-15 18:22:54 -0500

    Added Neccessary new files

--- Assets/scripts/GameStart.cs
+++ Assets/scripts/GameStart.cs
@@ -9,10 +9,12 @@ public class GameStart : MonoBehaviour
     public int sizez = 10;
     int startX = 0;
     int startY = 0;
-    public GameObject tile;
+    public GameObject blacktile;
+    public GameObject whitetile;
     public float spacing = .71f;
     float x = 0;
     float z = 0;
+    public Camera camera;
 
     // Use this for initialization
     void Start()
@@ -24,7 +26,27 @@ public class GameStart : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        { 
+            // if left button pressed...
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
 
+                if(hit.collider.gameObject.name.Equals("BlackTile(Clone)"))
+                { 
+                    Instantiate(whitetile, hit.collider.transform.position, hit.collider.transform.rotation);
+                }
+                else
+                {
+                    Instantiate(blacktile, hit.collider.transform.position, hit.collider.transform.rotation);
+                }
+                // the object identified by hit.transform was clicked
+                // do whatever you want
+                Destroy(hit.collider.gameObject);
+            }
+        }
     }
 
     public void PlaceMap()
@@ -38,7 +60,7 @@ public class GameStart : MonoBehaviour
         {
             for (int n = startY; n < sizex; n++)
             {
-                Instantiate(tile, new Vector3(x, 0, z), Quaternion.identity);
+                Instantiate(blacktile, new Vector3(x, 0, z), Quaternion.identity);
                 x = x + spacing;
             }
             x = 0;
